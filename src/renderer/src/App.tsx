3fc59baf15d55f379d82be4248767f99a3dfaa11
Adiul Alam Adil <adiulalam@gmail.com>
2023-02-28 03:28:38 +0000

    cleanup

--- src/renderer/src/App.tsx
+++ src/renderer/src/App.tsx
@@ -1,9 +1,6 @@
-import Versions from './components/Versions'
-
 function App(): JSX.Element {
   return (
     <div className="container">
-      <Versions></Versions>
       <h1 className="text-3xl font-bold underline">Hello world!</h1>
     </div>
   )
