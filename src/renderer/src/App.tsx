9ec6f3c5a7e8ad0ac1fd75f3c0899b8d54057dac
Adiul Alam Adil <adiulalam@gmail.com>
2023-03-05 00:12:50 +0000

    env

--- src/renderer/src/App.tsx
+++ src/renderer/src/App.tsx
@@ -1,7 +1,10 @@
 function App(): JSX.Element {
+  console.log(import.meta.env)
   return (
     <div className="container">
-      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+      <h1 className="text-3xl font-bold underline">
+        {import.meta.env.RENDERER_VITE_API_KEY} Hello world!
+      </h1>
     </div>
   )
 }
